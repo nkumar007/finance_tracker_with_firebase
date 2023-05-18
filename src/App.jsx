eaa7db6eda32b0542492d68cfc8ff02f26f23f7f
nkumar007 <naveen.mbuff98@gmail.com>
2023-05-18 14:06:13 +0530

    Setting Up pages with react-router-6

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// styles
 import "./App.css";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import SignUp from "./pages/signUp/SignUp";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/login", element: <Login /> },
+  {
+    path: "/signup",
+    element: <SignUp />,
+  },
+]);
 
 function App() {
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
